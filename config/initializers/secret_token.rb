c256a3451678537895a5d4638b28875a7ab6f964
KuroCrow <393323546@qq.com>
2012-05-01 21:05:26 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookstore::Application.config.secret_token = '31c1fa6eb35523eaaf84b2c916ba32e2425df9151b2ce09dcd91d912fe5bf54e1b60f3107f855a6409d610439d068da0deeffad3ace2ae4031b9377405e0aa50'
