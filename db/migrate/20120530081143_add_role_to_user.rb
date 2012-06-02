ea3234f55811fe8bdcfcd0fe9368667e85dda582
KuroCrow <393323546@qq.com>
2012-06-03 00:18:55 +0800

    modify addroletouser selfup

--- db/migrate/20120530081143_add_role_to_user.rb
+++ db/migrate/20120530081143_add_role_to_user.rb
@@ -1,5 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration
   def self.up
-    change_column :users, :role, :string, :default => 'Administrator'
+    add_column :users, :role, :string, :default => 'Administrator'
   end
 end
